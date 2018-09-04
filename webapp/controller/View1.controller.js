6e6dcd8876e6892841f379a90afa425a3c6f1893
myakinkii <myakinkii@gmail.com>
2018-09-04 23:19:46 +0300

    added carousel

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -2,7 +2,8 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"libs/nowjs/now",
-], function (Controller,JSONModel,now) {
+	"sap/m/Dialog","sap/m/Button"
+], function (Controller,JSONModel,now,Dialog,Button) {
 	"use strict";
 
 	return Controller.extend("com.minesnf.ui5client.controller.View1", {
@@ -10,14 +11,12 @@ sap.ui.define([
 		onInit:function(){
 			var mdl=new JSONModel({evts:{},msg:''});
 			this.getView().setModel(mdl);
-			window.now.dispatchEvent=function(e){e.ts=Date.now(); e.argTxt=JSON.stringify(e.arg); console.log(e); mdl.setProperty('/evts/'+e.ts,e)};
 			var self=this;
+			window.now.dispatchEvent=function(e){ self.processEvent.call(self,e) };
 			window.setTimeout(function(){ self.processCommand=window.now.processCommand; },500);
-this.getView().byId("input").attachBrowserEvent('keypress', function(e){
-     if(e.which == 13){
-         self.sendMsg.call(self);
-     }
-});
+			this.getView().byId("input").attachBrowserEvent('keypress', function(e){
+				if(e.which == 13){ self.sendMsg.call(self);
+			}});
 		},
 		
 		sendMsg:function(e){
@@ -25,6 +24,51 @@ this.getView().byId("input").attachBrowserEvent('keypress', function(e){
 			var msg=mdl.getProperty('/msg');
 			this.processCommand(msg);
 			mdl.setProperty('/msg','');
+		},
+
+		processEvent:function(e){
+			var mdl=this.getView().getModel();
+			e.ts=Date.now();
+			e.argTxt=JSON.stringify(e.arg); 
+			mdl.setProperty('/evts/'+e.ts,e);
+			if (this['on'+e.func]) this['on'+e.func](e);
+			// console.log(e);
+		},
+
+		onUpdateParties:function(e){
+			console.log("parties",e.arg);
+			this.getView().getModel().setProperty('/parties',e.arg);
+		},
+
+		onUpdatePlayers:function(e){
+			var players=[];
+			for (var p in e.arg) { e.arg[p].name=p; players.push(e.arg[p]); }
+			console.log("players",players);
+			this.getView().getModel().setProperty('/players',players);
+		},
+
+		startRank:function(e){
+			var boardSize=e.getSource().data().boardSize;
+			console.log(boardSize);
+			this.processCommand('/create rank '+boardSize);
+		},
+
+		onStartGame: function (e) {
+			var self = this;
+			var title=e.arg.boardId+" ("+e.arg.c+"x"+e.arg.r+")";
+			if (!this.pressDialog) {
+				this.pressDialog = new Dialog({
+					title: title,
+					content: [],
+					beginButton: new Button({
+						text: '{i18n>genericClose}',
+						press: function () { self.pressDialog.close(); self.processCommand("/quit"); }
+					})
+				});
+				this.getView().addDependent(this.pressDialog);
+			}
+			this.pressDialog.setTitle(title);
+			this.pressDialog.open();
 		}
 
 	});
