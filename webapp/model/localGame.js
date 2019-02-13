90706b8e15dcfdc6e6fb30812cbe9ffbc3a13da2
Myakinkii Alexey <myakinkii@gmail.com>
2019-02-13 15:23:06 +0000

    minor change

--- webapp/model/localGame.js
+++ webapp/model/localGame.js
@@ -280,6 +280,66 @@ sap.ui.define([], function () {
 			process.exit(0);
 	};
 
+	function RankGame(pars){
+		Game.call(this,pars);
+		this.bestTime=this.profiles[this.partyLeader][this.bSize];
+		this.gamesPlayed=0;
+		this.won=0;
+		this.lost=0;
+		this.winStreak=0;
+		this.loseStreak=0;
+	};
+  
+  	RankGame.prototype=new Game;
+  
+	RankGame.prototype.onStartBoard=function(){
+		this.resetScore();
+	};
+	
+	RankGame.prototype.onResetBoard=function(e){
+		this.gamesPlayed++;
+		if (e.win){
+		this.winStreak++;
+		this.loseStreak=0;
+		this.won++;
+		} else{
+		this.winStreak=0;
+		this.loseStreak++;
+		this.lost++;
+		}
+		var stat=this.getGenericStat();
+		stat.bestTime=this.bestTime;
+		stat.result=e.win?'win':'fail',
+		stat.gamesPlayed=this.gamesPlayed,
+		stat.won=this.won,
+		stat.lost=this.lost,
+		stat.winPercentage=Math.round(100*this.won/this.gamesPlayed)+'%',
+		stat.streak=this.winStreak?this.winStreak:this.loseStreak;
+		this.emitEvent('party',this.id,'game','ShowResultRank',stat);
+	};
+	
+	RankGame.prototype.onCells=function(re){
+		this.openCells(re.cells);
+	};
+	
+	RankGame.prototype.onBomb=function(re){
+		this.openCells(this.board.mines);
+		this.resetBoard(re);
+	};
+	
+	RankGame.prototype.onComplete=function(re){
+		this.openCells(re.cells);
+		this.openCells(this.board.mines);
+		re.win=1;
+		var time=this.now/1000;
+		if (!this.bestTime || time<this.bestTime){
+		this.bestTime=time;
+		this.emitEvent('server',null,null,'userNewBestTime',
+						{game:this.name,user:re.user,bSize:this.bSize,time:time,log:this.log});
+		}
+		this.resetBoard(re);
+	};
+	
 	function RPGGame(pars) {
 		Game.call(this, pars);
 		for (var u in this.players) if(!this.profiles[u]) this.profiles[u]={};
@@ -621,66 +681,6 @@ sap.ui.define([], function () {
 		this.openCells(re.cells);
 		this.openCells(this.board.mines);
 		if (!this.inBattle) this.startBattle();
-	};
-
-	function RankGame(pars){
-		Game.call(this,pars);
-		this.bestTime=this.profiles[this.partyLeader][this.bSize];
-		this.gamesPlayed=0;
-		this.won=0;
-		this.lost=0;
-		this.winStreak=0;
-		this.loseStreak=0;
-	};
-  
-  	RankGame.prototype=new Game;
-  
-	RankGame.prototype.onStartBoard=function(){
-		this.resetScore();
-	};
-	
-	RankGame.prototype.onResetBoard=function(e){
-		this.gamesPlayed++;
-		if (e.win){
-		this.winStreak++;
-		this.loseStreak=0;
-		this.won++;
-		} else{
-		this.winStreak=0;
-		this.loseStreak++;
-		this.lost++;
-		}
-		var stat=this.getGenericStat();
-		stat.bestTime=this.bestTime;
-		stat.result=e.win?'win':'fail',
-		stat.gamesPlayed=this.gamesPlayed,
-		stat.won=this.won,
-		stat.lost=this.lost,
-		stat.winPercentage=Math.round(100*this.won/this.gamesPlayed)+'%',
-		stat.streak=this.winStreak?this.winStreak:this.loseStreak;
-		this.emitEvent('party',this.id,'game','ShowResultRank',stat);
-	};
-	
-	RankGame.prototype.onCells=function(re){
-		this.openCells(re.cells);
-	};
-	
-	RankGame.prototype.onBomb=function(re){
-		this.openCells(this.board.mines);
-		this.resetBoard(re);
-	};
-	
-	RankGame.prototype.onComplete=function(re){
-		this.openCells(re.cells);
-		this.openCells(this.board.mines);
-		re.win=1;
-		var time=this.now/1000;
-		if (!this.bestTime || time<this.bestTime){
-		this.bestTime=time;
-		this.emitEvent('server',null,null,'userNewBestTime',
-						{game:this.name,user:re.user,bSize:this.bSize,time:time,log:this.log});
-		}
-		this.resetBoard(re);
 	};
 
 	return {Board:Board, RPGGame:RPGGame, RankGame:RankGame};
